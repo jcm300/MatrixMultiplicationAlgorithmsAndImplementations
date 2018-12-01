f4bf5e447c183be4cfbb6d86a9486e45f17eb149
jcm300 <zeclmartins@gmail.com>
2018-12-01 23:49:58 +0000

    matrix mult with another order of loops

---
+++ src/exe2_3_1.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+#define N 50
+
+// dot product of two matrices a and b, result saved in matrix c
+void dotProduct(float **c, float **a, float **b, int n){
+
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < n; k++){
+            for(int j = 0; j < n; j++){
+                c[i][j] += a[i][k] * b[k][j];
+            }       
+        }   
+    }   
+}
+
+//main function
+int main(){
+    unsigned seed=0;
+    float **a = (float**)malloc(sizeof(float*)*N);
+    float **b = (float**)malloc(sizeof(float*)*N);
+    float **c = (float**)malloc(sizeof(float*)*N);
+
+    srand(seed);
+
+    //build matrix A with random values and C initilized with 0's
+    for(int i = 0; i < N; i++){
+        c[i] = (float*) malloc(sizeof(float)*N);
+        a[i] = (float*) malloc(sizeof(float)*N);
+        for(int j = 0; j < N; j++){
+            a[i][j] = rand();
+            c[i][j] = 0;
+        }   
+    }
+
+    //build matrix B with all elements equals to 1
+    for(int i = 0; i < N; i++){
+        b[i] = (float*) malloc(sizeof(float)*N);
+        for(int j = 0; j < N; j++)
+            b[i][j] = 1;
+    }
+
+    dotProduct(c,a,b,N);
+
+    return 0;
+}
