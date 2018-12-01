4b38b7935e27a7d5c0bf2580a27aaf0f11bc9264
jcm300 <zeclmartins@gmail.com>
2018-12-01 16:33:25 +0000

    basic dot product of matrices

---
+++ src/exe2.2.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+#define N 50
+
+// dot product of two matrices a and b, result saved in matrix c
+void dotProduct(float **c, float **a, float **b, int n){
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            c[i][j] = 0;
+            for(int k = 0; k < n; k++){
+                c[i][j] += a[i][k] * b[k][j];
+            }       
+        }   
+    }   
+}
+
+//main function
+int main(){
+    unsigned seed;
+    float a[N][N], b[N][N], c[N][N];
+
+    srand(seed);
+
+    //build matrix A with random values
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < N; j++)
+            a[i][j] = rand();
+
+    //build matrix B with all elements equals to 1
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < N; j++)
+            b[i][j] = 1;
+
+    dotProduct(c,a,b,N);
+
+    return 0;
+}
