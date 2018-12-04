cfddbfd06dcd80373edb00883e54783a8a81c7d7
MQuaresma <miguelquaresma97@gmail.com>
2018-12-04 11:16:58 +0000

    Optimized memory access on exe2_3_2

--- src/exe2_3_2.c
+++ src/exe2_3_2.c
@@ -1,16 +1,42 @@
+#include<stdio.h>
 #include<stdlib.h>
 
-#define N 50
+#define N 4
+
+// Inplace transpose
+void transpose(float **m, int n){
+    float temp;
+    for(int i=0; i<n; i++)
+        for(int j=i; j<n; j++){
+            temp = m[i][j];
+            m[i][j] = m[j][i];
+            m[j][i] = temp;
+        }
+}
+
 
 
 void dotProduct(float **c, float **a, float **b, int n){
-    
     for(int j=0; j < n; j++)
         for(int k=0; k < n; k++)
             for(int i=0; i < n; i++)
                 c[i][j] += a[i][k] * b[k][j]; 
 }
 
+void dotProduct_RowOpt(float **c, float **a, float **b, int n){
+    for(int j=0; j < n; j++)
+        for(int k=0; k < n; k++)
+            for(int i=0; i < n; i++)
+                c[i][j] += a[k][i] * b[j][k]; 
+}
+
+void printMatrix(float **m, int n){
+    for(int i=0; i<n; i++){
+        for(int j=0;j<n;j++)
+            printf("%f ", m[i][j]);
+        printf("\n");
+    }
+}
 
 int main(int argc, char *argv[]){
     unsigned seed=0;
@@ -21,18 +47,26 @@ int main(int argc, char *argv[]){
     srand(seed);
 
     for(int i = 0; i < N; i ++){
-        c[i] = (float*)malloc(sizeof(float)*N);
-        a[i] = (float*)calloc(N, sizeof(float));
+        a[i] = (float*)malloc(sizeof(float)*N);
+        c[i] = (float*)calloc(N, sizeof(float));
         for(int j = 0; j < N; j ++)
             a[i][j] = rand();
     }
 
     for(int i = 0; i < N; i ++){
         b[i] = (float*)malloc(sizeof(float)*N);
-        for(int j = 0; j < N; i ++)
+        for(int j = 0; j < N; j ++)
             b[i][j] = 1;
     }
 
     dotProduct(c, a, b, N);
 
+    //reset matrix
+    for(int i = 0; i < N; i ++){
+        for(int j = 0; j < N; j ++)
+            c[i][j] = 0.f;
+    }
+    transpose(a,N);
+    transpose(b,N);
+    dotProduct_RowOpt(c, a, b, N); 
 }
