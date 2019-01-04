79ec74de4badf66628abb7655df9b74f0b1d813f
jcm300 <zeclmartins@gmail.com>
2019-01-04 00:23:37 +0000

    added CUDA version

--- kepler/run.sh
+++ kepler/run.sh
@@ -2,11 +2,12 @@
 #
 #PBS -N Teste
 #PBS -l walltime=30:00
-#PBS -l nodes=1:r662:ppn=24
+#PBS -l nodes=1:r662:ppn=48:k20
 #PBS -q mei
 
 module load gcc/5.3.0
 #module load gcc/7.2.0
+module load cuda/9.1
 
 cd teste
 rm Teste.*
@@ -15,11 +16,11 @@ make
 
 mkdir times
 
-NUM=0
-while [ $NUM -lt 8 ]; do
-    ./bin/dot_product >> times/dot_product
-    let NUM=NUM+1
-done
+#NUM=0
+#while [ $NUM -lt 8 ]; do
+#    ./bin/dot_product >> times/dot_product
+#    let NUM=NUM+1
+#done
 
 NUM=0
 while [ $NUM -lt 8 ]; do
@@ -27,14 +28,14 @@ while [ $NUM -lt 8 ]; do
     let NUM=NUM+1
 done
 
-NUM=0
-while [ $NUM -lt 8 ]; do
-    ./bin/dot_product_2_no_transpose >> times/dot_product_2_no_transpose
-    let NUM=NUM+1
-done
+#NUM=0
+#while [ $NUM -lt 8 ]; do
+#    ./bin/dot_product_2_no_transpose >> times/dot_product_2_no_transpose
+#    let NUM=NUM+1
+#done
 
-NUM=0
-while [ $NUM -lt 8 ]; do
-    ./bin/dot_product_2_transpose >> times/dot_product_2_transpose
-    let NUM=NUM+1
-done
+#NUM=0
+#while [ $NUM -lt 8 ]; do
+#    ./bin/dot_product_2_transpose >> times/dot_product_2_transpose
+#    let NUM=NUM+1
+#done
--- kepler/src/testing_utils.h
+++
@@ -1,9 +0,0 @@
-#include<sys/time.h>
-#include<stdio.h>
-#include<stdlib.h>
-
-#define TIME_RESOLUTION 1000000
-#define N 20
-
-void start (void);
-long long unsigned stop (void);
