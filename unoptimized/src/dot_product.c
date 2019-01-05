0388e78fb43f86aaef789580d48c79cc22237519
jcm300 <zeclmartins@gmail.com>
2019-01-05 03:18:22 +0000

    minor fixes

--- unoptimized/src/dot_product.c
+++ unoptimized/src/dot_product.c
@@ -3,14 +3,11 @@
 // dot product of two matrices a and b, result saved in matrix c
 void dotProduct(float **c, float **a, float **b, int n){
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            c[i][j] = 0;
-            for(int k = 0; k < n; k++){
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            for(int k = 0; k < n; k++)
                 c[i][j] += a[i][k] * b[k][j];
-            }       
-        }   
-    }   
+
 }
 
 // dot product of two matrices with block optimization
@@ -36,8 +33,10 @@ int main(){
     for(int i = 0; i < N; i++){
         c[i] = (float*) malloc(sizeof(float)*N);
         a[i] = (float*) malloc(sizeof(float)*N);
-        for(int j = 0; j < N; j++)
+        for(int j = 0; j < N; j++){
             a[i][j] = rand();
+            c[i][j] = 0;
+        }   
     }
 
     //build matrix B with all elements equals to 1
