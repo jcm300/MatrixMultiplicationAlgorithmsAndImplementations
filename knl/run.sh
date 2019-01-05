fa142f6c9b9ed62f7cd4871810a5771723bb1529
jcm300 <zeclmartins@gmail.com>
2019-01-05 16:20:19 +0000

    knl updated

--- knl/run.sh
+++ knl/run.sh
@@ -1,40 +1,14 @@
 #!/bin/sh
-#
-#PBS -N Teste
-#PBS -l walltime=30:00
-#PBS -l nodes=1:r662:ppn=48
-#PBS -q mei
 
-module load gcc/5.3.0
-#module load gcc/7.2.0
+export PATH=/share/apps/gcc/5.3.0/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/gcc/5.3.0/lib:$LD_LIBRARY_PATH
+source /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh intel64
 
-cd teste
-rm Teste.*
-make clean
 make
-
 mkdir times
 
 NUM=0
 while [ $NUM -lt 8 ]; do
-    ./bin/dot_product >> times/dot_product
-    let NUM=NUM+1
-done
-
-NUM=0
-while [ $NUM -lt 8 ]; do
     ./bin/dot_product_1 >> times/dot_product_1
-    let NUM=NUM+1
-done
-
-NUM=0
-while [ $NUM -lt 8 ]; do
-    ./bin/dot_product_2_no_transpose >> times/dot_product_2_no_transpose
-    let NUM=NUM+1
-done
-
-NUM=0
-while [ $NUM -lt 8 ]; do
-    ./bin/dot_product_2_transpose >> times/dot_product_2_transpose
     let NUM=NUM+1
 done
