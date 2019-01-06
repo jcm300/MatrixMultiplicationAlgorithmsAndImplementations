53d2b4a798f78bde6428e33bbc630effb67f868a
MQuaresma <miguelquaresma97@gmail.com>
2019-01-06 08:45:42 +0000

    Counters for RAM access metrics

--- counters/src/dot_product_1.c
+++ counters/src/dot_product_1.c
@@ -1,9 +1,7 @@
 #include "testing_utils.h"
 
-int Events[NUM_EVENTS]={PAPI_L1_LDM,PAPI_L1_DCM};
-//int Events[NUM_EVENTS]={PAPI_L2_STM,PAPI_L2_DCM};
-//int Events[NUM_EVENTS]={PAPI_L3_DCR,PAPI_L3_TCM};
-//int Events[NUM_EVENTS]={PAPI_L3_ICA,PAPI_L3_ICR};
+int Events[NUM_EVENTS]={PAPI_LD_INS, PAPI_SR_INS};
+//int Events[NUM_EVENTS]={PAPI_TOT_INS,PAPI_L3_TCM};
 int retval;
 long long values[NUM_EVENTS];
 
